dbc0ea1939c2ff197d5e1d2af02ec1ce9a0c2a46
Matheus Mathias <mathias.matheus@ymail.com>
2026-08-24 10:23:04 -0300

    refact: remove unused cron property from plugin version

--- version.php
+++ version.php
@@ -27,6 +27,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2026082000;        // The current module version (Date: YYYYMMDDXX).
 $plugin->requires  = 2024100100;        // Requires this Moodle version (Moodle 4.5 approx release date).
 $plugin->component = 'mod_dialoguebuilder'; // Full name of the plugin (used for diagnostics).
-$plugin->cron      = 0;
 $plugin->maturity  = MATURITY_ALPHA;
 $plugin->release   = '0.1.0';
